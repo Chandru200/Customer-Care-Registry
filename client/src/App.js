3b21cfd2b6521c1d7b6c148be352a0333dfce3d1
Chan_Dru <chandru.hippovideo.io>
2022-10-23 19:36:30 +0530

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Registration from './components/Register'
+import Login from "./components/login";
+import Dashboard from './components/dashboard'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+
+
+<Router>
+           <div className="App">
+            
+           <Routes>
+                 <Route exact path='/' element={< Login />}></Route>
+                 <Route exact path='/registration' element={<Registration />}></Route>
+                 <Route exact path='/dashboard' element={< Dashboard />}></Route>
+          </Routes>
+          </div>
+       </Router>
+  )}
+
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+export default function Registration() {
+    return(
+
+    <div>
+       Registration
+       {/* <Navigate to="/dashboard" /> */}
+    </div>
+
+    )
+}
